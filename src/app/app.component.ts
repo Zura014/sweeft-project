ebf8d06bea9bb79c0b5cf5434f6fb05e46da8107
Zura014 <zuragagnidze50@gmail.com>
2025-02-03 16:26:52 +0400

    Remove MatProgressBar import from AppComponent and eliminate delay in recipe resolver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MatProgressBar],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
